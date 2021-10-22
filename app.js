00c4af174c8d12a4500c41b8a8b9e96c82d933d5
ccarver80 <ckc_80@outlook.com>
2021-10-21 22:25:02 -0600

    Added images, and built some static files

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const data = require("./data/data.json");
 
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
+const { render } = require("pug");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/static', express.static("public"));
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-    res.send()
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("projects", (res, req) => {
+  res.render("project");
+});
 
 app.listen(3000, () => {
   console.log("Your server is running on port 3000!");
